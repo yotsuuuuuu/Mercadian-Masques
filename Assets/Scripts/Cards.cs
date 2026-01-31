b6c7ea826d094a7d76a553cadcb37b2ca7aa174c
LAPTOP-1NGMDTUL\minec <vinaddante@gmail.com>
2026-01-30 20:42:36 -0500

    basic card class

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static Cards;
+
+
+
+public enum maskType { Bull, Frog, Deer, Bird, Snake };
+
+public enum dir { north, east, sout, west };
+
+
+[System.Serializable] public struct CardMove
+{
+    public dir direction;
+    public int amount;
+}
+public class Cards:MonoBehaviour
+{
+   
+    public Queue<CardMove>cardMovement;
+    [SerializeField]private List<CardMove> cardMovementList;
+    [SerializeField] public maskType mask;
+    [SerializeField] public dir direction;
+
+    private void Awake()
+    {
+        cardMovement = new Queue<CardMove>(cardMovementList);
+    }
+
+}
