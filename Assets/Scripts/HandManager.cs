02f8cbfc4299d34213e72deb8e305b18945731cf
vaddante <vinaddante@gmail.com>
2026-01-31 14:00:45 -0500

    cahnged prefab logic in handmanager still flawed and needs to be worked on

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -7,7 +7,7 @@ public class HandManager : MonoBehaviour
     [SerializeField] public GameObject cardPrefab;
     public Transform handTransform;
     public int handsize;
-    public float spread = -5.0f;
+    public float spread = 50.0f;
     float spacing = 100;
     float vspacing = 100;
     public List<Cards> cardsInHand = new List<Cards>();
@@ -18,6 +18,7 @@ public class HandManager : MonoBehaviour
 
     private void Start()
     {
+        cardstest.Add(cardPrefab.GetComponent<Cards>());
         cardstest.Add(cardPrefab.GetComponent<Cards>());
         AddCardsToHand(cardstest);
 
@@ -33,7 +34,7 @@ public class HandManager : MonoBehaviour
        
        for(int i = 0;cardslist_.Count > i;i++)
         {
-            GameObject newCardGO = Instantiate(cardPrefab, handTransform);
+            GameObject newCardGO = Instantiate(cardPrefab, handTransform.position,Quaternion.identity,handTransform);
             Cards cardUI = newCardGO.GetComponent<Cards>();
 
             cardUI.mask = cardslist_[i].mask;
@@ -47,16 +48,22 @@ public class HandManager : MonoBehaviour
     private void UpdateHandVisual()
     {
         cardAmount = cardsInHand.Count;
+        if(cardAmount == 1)
+        {
+            cardsInHand[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            cardsInHand[0].transform.localPosition = new Vector3(0f, 0f, 0f);
+            return;
+        }
         for (int i = 0; i < cardAmount; i++)
         {
             float rotationAngle = (spread * (i - (cardAmount - 1) / 2f));
             cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
 
-            float horizontalOffset = (spacing * (i - (cardAmount - 1) / 2f));
+            //float horizontalOffset = (spacing * (i - (cardAmount - 1) / 2f));
 
-            float normalizedpos = (2f * i / (cardAmount - 1) - 1f);
-            float verticlOffset = vspacing * (i - normalizedpos*normalizedpos);
-            cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticlOffset, 0f);
+            //float normalizedpos = (2f * i / (cardAmount - 1) - 1f);
+            //float verticlOffset = vspacing * (i - normalizedpos*normalizedpos);
+            //cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticlOffset, 0f);
         }
     }
     
