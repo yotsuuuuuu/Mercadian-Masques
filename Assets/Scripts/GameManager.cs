2fd26e5f542033ad714dac12c52435e58f541800
Kykiwi <kevin.valencia@live.ca>
2026-01-31 14:01:39 -0500

    small update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,8 +23,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject boardManager;
     [SerializeField] private GameObject handManager;
     [SerializeField] public LevelCardDataResource CardsData;
-    [SerializeField] public LevelDataResource ListOfChunks;
+    [SerializeField] public LevelDataResource ChunkData;
+    [SerializeField] private Player player;
     private List<CardData> ListofCardData;
+    
+    int boardSizeX = 9; // default values to for the board
+    int boardSizeZ = 9;
+    int boardSizeY = 2; 
 
     public struct CardData
     {
@@ -37,9 +42,11 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //board = boardManager.GetComponent<BoardManager>();
+        
+        board = boardManager.GetComponent<BoardManager>();
         //hand = handManager.GetComponent<HandManager>();
         ListofCardData = PopulateCardData(CardsData.ListOfCards);
+        //board.Initilize(ArrayofChunks(ChunkData.chunks), boardSizeX, boardSizeY, boardSizeZ);
 
         isProcessingCard = false;
 
@@ -57,6 +64,18 @@ public class GameManager : MonoBehaviour
         //stateMachine.AddTransitions(processing, new TransitonState(() => !ProcessingCard(), processing));
         stateMachine.AddTransitions(playerState, new TransitonState(() => PlayerCheck(), idel));
         stateMachine.AddTransitions(playerState, new TransitonState(() => !PlayerCheck(), processing));
+    }
+
+    private int[,,] ArrayofChunks(List<ChunkLevelData> chunks)
+    {
+        int[,,] array = new int[boardSizeX, boardSizeY, boardSizeZ];
+        for(int i = 0; i < chunks.Count; i++)
+        {
+            Vector3Int pos = chunks[i].position;
+            array[pos.x, pos.y, pos.z] = (int)chunks[i].type;
+        }
+
+        return array;
     }
 
     private List<CardData> PopulateCardData(List<CardLevelData> listOfCards)
@@ -101,6 +120,7 @@ public class GameManager : MonoBehaviour
     public void AddCard(Queue<CardMove> movelist)
     {
         // TODO : waiting on Board 
+        // check  current player orientation
         // ACTION ALL
 
         isProcessingCard = true;
@@ -114,7 +134,6 @@ public class GameManager : MonoBehaviour
     // NEED ALSO TO CHECK HAND SIZE
     public bool HasPlayerLost() { return false; }
 
-    //TODO :  IMPLEMENT NOT WAITING ON ANYTHING
     public void ResetLevel() {
         Debug.Log("Resetting Level");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
